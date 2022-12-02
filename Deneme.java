920cb53e1dd92ad0cfda80e351f521acdfca6e76
ismetcanbyk <ismetcanbyk@gmail.com>
2022-12-02 21:17:54 +0300

    NYP lab ödevi

---
+++ Deneme.java
@@ -0,0 +1,13 @@
+public class Deneme {
+    public static void main(String[] args) {
+      
+        Kasiyer dsa = new Kasiyer();
+        dsa.isVakti();
+        
+        Kasiyer k2 =new Kasiyer("can");
+
+        k2.isVakti();
+        
+    }
+}
+
